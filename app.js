9ffd7d86f8f79ccefd210737d8b0465de30ca48b
Mc Karlwayne Jeff Manalo <161920948+McKarlwayne0317@users.noreply.github.com>
2024-04-07 16:14:01 +0800

    3rd commit with level 3 protection using md5

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
-
+require('dotenv').config()
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const ejs = require('ejs');
-const encrypt = require('mongoose-encryption');
+const md5 = require('md5');
+
 
 const app = express();
 app.use(express.static("public"));
@@ -19,9 +20,7 @@ const userSchema = new mongoose.Schema({
     password: String
 });
 
-//Encryption declaration
-const secret = "ItoAyIsangSekreto";
-userSchema.plugin(encrypt,{secret:secret, encryptedFields: ['password']});
+
 
 //User Model
 const User = new mongoose.model("User", userSchema);
@@ -41,7 +40,7 @@ app.route("/login")
         User.findOne({email:req.body.username}).exec()
             .then(function(foundUser){
                 if(foundUser){
-                    if(foundUser.password === req.body.password){
+                    if(foundUser.password === md5(req.body.password)){
                         res.render("secrets")
                         console.log("Successfully logged in as "+foundUser.email);
                     }else{
@@ -66,7 +65,7 @@ app.route("/register")
     .post(function(req,res){
         const newUser = new User({
             email:req.body.username,
-            password:req.body.password
+            password:md5(req.body.password)
         });
 
         newUser.save()
