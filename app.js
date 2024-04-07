c702fc3d8c7a9c18138a5c857b94284acaba4fc0
Mc Karlwayne Jeff Manalo <161920948+McKarlwayne0317@users.noreply.github.com>
2024-04-07 15:56:48 +0800

    First commit here at secrets implementation of mongoose-encrypt

---
+++ app.js
@@ -0,0 +1,86 @@
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const ejs = require('ejs');
+const encrypt = require('mongoose-encryption');
+
+const app = express();
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended:true}));
+app.set('view engine', 'ejs');
+
+//Connection string to mongooseXmongoDB
+mongoose.connect("mongodb://localhost:27017/userDB");
+
+//User Schema
+const userSchema = new mongoose.Schema({
+    email: String,
+    password: String
+});
+
+//Encryption declaration
+const secret = "ItoAyIsangSekreto";
+userSchema.plugin(encrypt,{secret:secret, encryptedFields: ['password']});
+
+//User Model
+const User = new mongoose.model("User", userSchema);
+
+//Home Page route
+app.route("/")
+    .get(function(req,res){
+res.render("home")
+    })
+
+//Login Page route
+app.route("/login")
+    .get(function(req,res){
+        res.render("login")
+    })
+    .post(function(req,res){
+        User.findOne({email:req.body.username}).exec()
+            .then(function(foundUser){
+                if(foundUser){
+                    if(foundUser.password === req.body.password){
+                        res.render("secrets")
+                        console.log("Successfully logged in as "+foundUser.email);
+                    }else{
+                        res.render("login");
+                        console.log("Incorrect password!!");
+                    }
+                }else{
+                    res.render("login");
+                    console.log("User not found!!" +foundUser.email);
+                }
+            })
+        .catch(function(err){
+            console.log(err);
+        })
+    })
+
+//Register Page route
+app.route("/register")
+    .get(function(req,res){
+        res.render("register")
+    })
+    .post(function(req,res){
+        const newUser = new User({
+            email:req.body.username,
+            password:req.body.password
+        });
+
+        newUser.save()
+            .then(function(savedUser){
+                res.render("secrets")
+                console.log("Successfully registered a new user, " + savedUser)
+            })
+            .catch(function(err){
+                console.log(err)
+            });
+    })
+
+
+app.listen(process.env.PORT || 3000, function(){
+    console.log("Server is up and running");
+})
+
